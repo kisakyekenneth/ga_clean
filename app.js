fc4a2eb0b183ee4d65b3e400d11205aa97f5daf0
kiken-eng <kanyecope@gmail.com>
2021-03-19 10:57:23 +0300

    Minor changes here

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 //Imports
 const express = require("express");
+require("dotenv").config(); //Required to work with .env file
 
 //Instantiation
 const app = express();
 
 //Middlewares
-app.use('/static', express.static("public")); //Serve static files like css, js.
+app.use(express.static("public")); //Serve static files like css, js.
 app.use(express.static("files"));
 
 //Set view Enginee
@@ -14,12 +15,12 @@ app.set("views", "./views");
 
 //Routes
 app.get("/", (req, res) => {
-  res.render("login");
+  res.render("login", { title: "Login" }); //Dynamically passing the page title as object
 });
 
 app.get("/home", (req, res) => {
-  res.render("homepage");
-})
+  res.render("homepage", { title: "createEmployee" });
+});
 
 app.listen(process.env.HOST_PORT, () => {
   console.log("Listening on port ", process.env.HOST_PORT);
