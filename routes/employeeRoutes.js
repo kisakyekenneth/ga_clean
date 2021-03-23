1766a262ce5ce43c9205f89538783b801f559692
kiken-eng <kanyecope@gmail.com>
2021-03-23 10:02:36 +0300

    Added code to handle image uploads

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -1,10 +1,33 @@
-const express = require('express');
+const express = require("express");
 const route = express.Router();
+const multer = require('multer');
 
 route.get("/", (req, res) => {
-    res.render("createEmployee", {
-        title: "Employee Registration"
-    });
+  res.render("createEmployee", {
+    title: "Employee Registration"
+  });
+});
+
+// image upload
+var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
+});
+var upload = multer({
+  storage: storage
+});
+
+route.post("/", upload.single("imageupload"), (req, res) => {
+  try {
+    console.log(req.file);
+    res.send(req.file);
+  } catch (err) {
+    res.send(400);
+  }
 });
 
 module.exports = route;
