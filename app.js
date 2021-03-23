307be849e9b8018f6f840d12d668cd44c07f81d9
kiken-eng <kanyecope@gmail.com>
2021-03-23 09:55:02 +0300

    Added the route for non-existing routes

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 //Imports
 const express = require("express");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 require("dotenv").config(); //Required to work with .env file
+
+//Route middlewares
+var employeeRoute = require("./routes/employeeRoutes");
+var homeRoutes = require("./routes/homeRoutes");
+var userRoutes = require("./routes/userRoutes");
+var customerRoutes = require("./routes/customerRoutes");
 
 //Instantiation
 const app = express();
@@ -11,27 +17,26 @@ const app = express();
 app.use(express.static("public")); //Serve static files like css, js.
 app.use(express.static("files"));
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
-
-//Route middlewares
-const employeeRoute = require('./routes/employeeRoutes');
-const homeRoutes = require('./routes/homeRoutes');
-const userRoutes = require('./routes/userRoutes');
-const customerRoutes = require('./routes/customerRoutes');
-
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
 
 //Set view Enginee
 app.set("view engine", "pug");
 app.set("views", "./views");
 
-//        Routes 
-app.use("/employeeRegister", employeeRoute);
+//        Routes
 app.use("/", homeRoutes);
 app.use("/users", userRoutes);
+app.use("/employeeRegister", employeeRoute);
 app.use("/customer", customerRoutes);
 
+//Non existing routes
+app.get("*", (req, res) => {
+  res.send("Error Page, route doesnot exist")
+})
 
 //Mongo database connection using Mongoose
 mongoose.connect(
@@ -42,7 +47,6 @@ mongoose.connect(
     console.log("Connected to the DB");
   }
 );
-
 
 app.listen(process.env.PORT, () => {
   console.log("Listening on port ", process.env.PORT);
