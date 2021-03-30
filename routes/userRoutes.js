950e935ab568e9eea815f4f1db4f1720df4fdedd
kiken-eng <kanyecope@gmail.com>
2021-03-31 00:11:19 +0300

    Added logout route to destroy session and logout

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,61 +1,76 @@
 const express = require("express");
-const mongoose = require('mongoose');
-require('../models/users');
-const User = mongoose.model('User')
+const mongoose = require("mongoose");
+require("../models/users");
+const User = mongoose.model("User");
 
-const passport = require('passport');
-
+const passport = require("passport");
 
 const route = express.Router();
 const app = express();
-app.use(express.urlencoded({
+app.use(
+  express.urlencoded({
     extended: true
-}));
-
+  })
+);
 
 // checks username and password using passport
-route.post('/login', passport.authenticate('local', {
-    failureRedirect: '/'
-}), (req, res) => {
+route.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/"
+  }),
+  (req, res) => {
     req.session.user = req.user;
-    res.redirect('/customer');
-})
+    res.redirect("/customer");
+  }
+);
 
 //Register a user
 route.post("/", async (req, res) => {
-    try {
+  try {
+    // let passwd = await bcrypt.hash(req.body.password, 8)
+    // let customer_name = req.body.username;
 
-        // let passwd = await bcrypt.hash(req.body.password, 8)
-        // let customer_name = req.body.username;
+    // const user_data = new User({
+    //     username: req.body.username,
+    //     email: req.body.email,
+    //     password: req.body.password
+    // });
 
-        // const user_data = new User({
-        //     username: req.body.username,
-        //     email: req.body.email,
-        //     password: req.body.password
-        // });
+    // await user_data.save(() => {
+    //     //res.send("Successful")
+    //     res.redirect('/')
+    // })
 
-        // await user_data.save(() => {
-        //     //res.send("Successful")
-        //     res.redirect('/')
-        // })
-
-        const registrationDetails = new User(req.body);
-        await User.register(registrationDetails, req.body.password, (err) => {
-            if (err) {
-                throw err
-            }
-            res.redirect('/')
-        });
-    } catch (error) {
-        res.status(400).send('Sorry! Something went wrong.')
-        console.log(error)
-    }
+    const registrationDetails = new User(req.body);
+    await User.register(registrationDetails, req.body.password, err => {
+      if (err) {
+        throw err;
+      }
+      res.redirect("/");
+    });
+  } catch (error) {
+    res.status(400).send("Sorry! Something went wrong.");
+    console.log(error);
+  }
 });
 
+//logout
+route.get("/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.status(400).send("Unable to log out");
+      } else {
+        req.logout();
+        res.redirect("/");
+      }
+    });
+  }
+});
 
 route.get("*", (req, res) => {
-    res.send("Please check your specified path");
+  res.send("Please check your specified path");
 });
-
 
 module.exports = route;
