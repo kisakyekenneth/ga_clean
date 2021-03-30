3391a71e60330221c54776bdb2174991e558c833
kiken-eng <kanyecope@gmail.com>
2021-03-31 02:00:24 +0300

    Added check for sessions and update routes

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const route = express.Router();
 
-
-
 route.get("/", (req, res) => {
-    if (req.session.user) {
-        res.render("customerList");
-    } else {
-        res.redirect("/");
-    }
+  if (req.session.user) {
+    res.render("customerList");
+  } else {
+    res.redirect("/");
+  }
 });
 
 route.get("/calls", (req, res) => {
+  if (req.session.user) {
     res.render("customerCalls", {
-        title: "Record customer calls"
+      title: "Record customer calls"
     });
+  } else {
+    res.redirect("/");
+  }
 });
-
-
 
 module.exports = route;
