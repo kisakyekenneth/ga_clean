8aa7ceb65c9484acd3d72f6614c3be375dcd76b0
kiken-eng <kanyecope@gmail.com>
2021-03-30 23:20:56 +0300

    Adding the req.session.user to check if user logged in

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const route = express.Router();
 
+
+
 route.get("/", (req, res) => {
-    res.render("customerList");
+    if (req.session.user) {
+        res.render("customerList");
+    } else {
+        res.redirect("/");
+    }
 });
 
 route.get("/calls", (req, res) => {
