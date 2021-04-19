a166288c1256faf193525e152a3557ebd6eccb25
kiken-eng <kanyecope@gmail.com>
2021-04-20 01:54:18 +0300

    Added the validation for register form

--- public/js/login.js
+++ public/js/login.js
@@ -9,3 +9,46 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
     container.classList.remove("right-panel-active");
 });
+
+const username = document.userRegister.username;
+
+const email = document.userRegister.email;
+
+const password = document.userRegister.password;
+
+const display = document.getElementById('error-msg')
+
+let registerValidator = () => {     
+    
+     if (username.value == "") {
+         display.style.color = 'red'
+         display.innerHTML = 'Please fill all the inputs'
+         username.style.border = '1px solid red'
+         
+         username.focus();
+         
+         return false;
+     }
+    if (username.value != "") {
+         username.style.border = '1px solid #eee'
+    }
+     if (email.value == "") {
+         display.style.color = 'red'
+         display.innerHTML = 'Please fill all the inputs'
+         
+         email.style.border = '1px solid red'
+         email.focus();
+         
+         return false;
+    }
+    if (password.value == "") {
+         display.style.color = 'red'
+         display.innerHTML = 'Please fill all the inputs'
+         
+         password.style.border = '1px solid red'
+         password.focus();
+         
+         return false;
+     }
+     
+ }
